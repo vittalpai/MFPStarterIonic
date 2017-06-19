18c1276a2d3edf64a63096f8951e48d5825a39f2
m67hoff <mhoff@web.de>
2017-06-19 09:52:14 +0200

    Adapter postData sample

--- mobilefirst/MFPStarterIonicAdapter/src/main/adapter-resources/js/MFPStarterIonicAdapter-impl.js
+++ mobilefirst/MFPStarterIonicAdapter/src/main/adapter-resources/js/MFPStarterIonicAdapter-impl.js
@@ -1,6 +1,8 @@
 
 // Call http://www.jsontest.com/ Mock Service
 function getJsonTest() {
+	MFP.Logger.warn('--> MFPStarterIonicAdapter getJsonTest')
+	
 	var input = {
 		method: 'get',
 		returnedContentType: 'json',
@@ -11,6 +13,8 @@ function getJsonTest() {
 
 // Local data mock
 function getData() {
+	MFP.Logger.warn('--> MFPStarterIonicAdapter getData')
+	
 	var mock = {
 		user: 'Michael Hoffmann',
 		email: 'mhoff@de.ibm.com',
@@ -21,5 +25,23 @@ function getData() {
 
 // unprotected Hello world
 function hello() {
+	MFP.Logger.warn('--> MFPStarterIonicAdapter hello')
+	
 	return { result: "Hello from MFPStarterIonicAdapter" };
+}
+
+// send data
+// http://mobilefirstplatform.ibmcloud.com/tutorials/en/foundation/8.0/application-development/resource-request/javascript/
+// for get:  	resourceRequest.setQueryParameter("params", "['value1', 'value2']");
+// for post:  	var formParams = {"params":"['value1', 'value2']"};
+//				resourceRequest.sendFormParameters(formParams);
+function postData(p1,p2) {
+	MFP.Logger.warn('--> MFPStarterIonicAdapter postData')
+	MFP.Logger.warn('--> MFPStarterIonicAdapter postData: ' + p1 + ' ' + p2) 
+	
+	var test = {
+		para1: p1,
+		para2: p2,
+	};
+	return test;
 }
